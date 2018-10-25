99e0dae30beabfcf07df00af3e35113dafe84f99
p2355112 <Phil.Kazmier@Charter.com>
2018-10-25 14:03:15 -0600

    Added rough gallery POC

--- src/app/gallery/sundayraces.component.ts
+++ src/app/gallery/sundayraces.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @Component({
@@ -9,15 +10,36 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class SundayRacesComponent implements OnInit {
   closeResult: string;
+  imageName: string;
+
   constructor(private modalService: NgbModal) { }
+
+  
 
   ngOnInit() {
 
   }
 
-  open(content) {
-    console.log(content);
-    this.modalService.open(content).result.then((result) => {
+  open(content, idx) {
+    //var image = document.getElementById("1");
+    console.log(idx);
+    let images: Array<string> = new Array();
+    images.push("../../assets/BowMarSundayRaces_08262018/file1-7.jpeg");
+    images.push("../../assets/BowMarSundayRaces_08262018/file2-4.jpeg");
+    images.push("../../assets/BowMarSundayRaces_08262018/file3-2.jpeg");
+    images.push("../../assets/BowMarSundayRaces_08262018/file4-2.jpeg");
+    images.push("../../assets/BowMarSundayRaces_08262018/file5-2.jpeg");
+    images.push("../../assets/BowMarSundayRaces_08262018/file6-2.jpeg");
+    images.push("../../assets/BowMarSundayRaces_08262018/file7-2.jpeg");
+    images.push("../../assets/BowMarSundayRaces_08262018/file8-1.jpeg");
+    images.push("../../assets/BowMarSundayRaces_08262018/file9-1.jpeg");
+    images.push("../../assets/BowMarSundayRaces_08262018/file10-1.jpeg");
+    images.push("../../assets/BowMarSundayRaces_08262018/file11-1.jpeg");
+    images.push("../../assets/BowMarSundayRaces_08262018/file12-1.jpeg");
+    images.push("../../assets/BowMarSundayRaces_08262018/file-15.jpeg");
+    this.imageName = images[idx];
+
+    this.modalService.open(content, {size: 'lg'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
